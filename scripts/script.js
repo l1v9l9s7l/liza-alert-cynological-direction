4e02f6aa80ed55eae96026cd8b4c4711de8f7501
l1v9l9s7l <l1v9l9s7l@gmail.com>
2022-05-24 20:12:58 +0300

    V2

--- scripts/script.js
+++
@@ -1,8 +0,0 @@
-const filterBlockHead = document.querySelectorAll(".sidebar__block-head");
-
-filterBlockHead.forEach(item => {
-	item.addEventListener("click", function (evt) {
-		evt.stopPropagation();
-		item.parentNode.classList.toggle("sidebar__block_active");
-	});
-});
