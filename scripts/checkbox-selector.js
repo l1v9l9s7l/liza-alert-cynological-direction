1eb305ceb92b02a6ee1af6279df145dbd639501b
Алексей <avmakarov.mail@yandex.ru>
2022-05-24 23:54:28 +0300

    Реализована подсветка лейбла при выборе чекбокса

---
+++ scripts/checkbox-selector.js
@@ -0,0 +1,16 @@
+const checkboxes = document.querySelectorAll('.checkbox');
+
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('click', highlightLabel);
+});
+
+function highlightLabel(event) {
+    const checkbox = event.target;
+    const checkboxId = checkbox.id;
+    const label = document.querySelector('label[for=' + checkboxId + ']');
+    if (checkbox.checked === true) {
+        label.classList.add('question__answer-text_chose');
+    } else {
+        label.classList.remove('question__answer-text_chose');
+    }
+}
