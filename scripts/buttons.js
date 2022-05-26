37047f233b9194d81d0a044e84049bb392ab5395
Ivan Ershov <Navi133@yandex.ru>
2022-05-26 21:38:46 +0300

    Дорабатан файл buttons.js

---
+++ scripts/buttons.js
@@ -0,0 +1,14 @@
+const route = {
+	test_preview: '../test2.html',
+	about_test: '../about-test.html',
+	buttons: '../buttons.html',
+};
+const buttons = document.querySelectorAll('.btn');
+
+buttons.forEach(button => {
+	const url = route[button.id];
+	button.url = url;
+	button.addEventListener('click', (evt) => {
+		window.location.href = url;
+	});
+});
