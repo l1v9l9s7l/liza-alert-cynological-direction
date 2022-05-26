70f0f1310656f3cfc1f042cc6921d442b52c094b
Алексей <avmakarov.mail@yandex.ru>
2022-05-26 23:05:40 +0300

    Создание скрипта управления radio

--- scripts/radio-selector.js
+++ scripts/radio-selector.js
@@ -1,33 +1,29 @@
 const radios = document.querySelectorAll('.radio');
 const radioGroup = Array.from(radios).reduce((prev, radio) => {
-    console.log('Starts');
-	console.log(`RadioName:${radio.name}`);
-    console.log(prev);
-	console.log('prevUp')
-    if (prev[radio.name] === undefined) {
-        console.log('If');
-        return prev[radio.name] = [radio.id];
+    if (radio.name in prev) {
+        prev[radio.name] = [...prev[radio.name], radio.id];
+    } else {
+        prev[radio.name] = [radio.id];
     }
-    console.log('afterIf', prev);
-    return prev[radio.name].push(radio.id);
+    return prev;
 }, {});
-console.log(radioGroup);
 
 radios.forEach(radio => {
-    radio.addEventListener('click', highlightLabel);
+    radio.addEventListener('click', highlightRadioLabel);
 });
 
-function highlightLabel(event) {
+function highlightRadioLabel(event) {
     const radio = event.target;
     const radioId = radio.id;
-    console.log(radioId);
     const label = document.querySelector('label[for=' + radioId + ']');
 
-    console.log(label);
-    console.log(radio);
     if (radio.checked) {
         label.classList.add('question__answer-text_chose');
-    } else {
-        label.classList.remove('question__answer-text_chose');
     }
+
+    const filterIds = radioGroup[radio.name].filter(element => element !== radio.id);
+
+    filterIds.forEach(id => {
+        document.querySelector('label[for=' + id + ']').classList.remove('question__answer-text_chose');
+    });
 }
