9820049b4b1d32f184b5f17d9fd444f8431db89a
Алексей <avmakarov.mail@yandex.ru>
2022-05-26 21:21:52 +0300

    добавлено содержание на страницу позитивного теста

--- scripts/radio-selector.js
+++ scripts/radio-selector.js
@@ -1,4 +1,17 @@
 const radios = document.querySelectorAll('.radio');
+const radioGroup = Array.from(radios).reduce((prev, radio) => {
+    console.log('Starts');
+	console.log(`RadioName:${radio.name}`);
+    console.log(prev);
+	console.log('prevUp')
+    if (prev[radio.name] === undefined) {
+        console.log('If');
+        return prev[radio.name] = [radio.id];
+    }
+    console.log('afterIf', prev);
+    return prev[radio.name].push(radio.id);
+}, {});
+console.log(radioGroup);
 
 radios.forEach(radio => {
     radio.addEventListener('click', highlightLabel);
@@ -7,7 +20,9 @@ radios.forEach(radio => {
 function highlightLabel(event) {
     const radio = event.target;
     const radioId = radio.id;
+    console.log(radioId);
     const label = document.querySelector('label[for=' + radioId + ']');
+
     console.log(label);
     console.log(radio);
     if (radio.checked) {
