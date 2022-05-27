9d4dac7f48cc94f9ac80cbf47d9ed76210a0eb2b
l1v9l9s7l <l1v9l9s7l@gmail.com>
2022-05-27 20:59:17 +0300

    V1.1

---
+++ scripts/radio-selector.js
@@ -0,0 +1,29 @@
+const radios = document.querySelectorAll('.radio');
+const radioGroup = Array.from(radios).reduce((prev, radio) => {
+    if (radio.name in prev) {
+        prev[radio.name] = [...prev[radio.name], radio.id];
+    } else {
+        prev[radio.name] = [radio.id];
+    }
+    return prev;
+}, {});
+
+radios.forEach(radio => {
+    radio.addEventListener('click', highlightRadioLabel);
+});
+
+function highlightRadioLabel(event) {
+    const radio = event.target;
+    const radioId = radio.id;
+    const label = document.querySelector('label[for=' + radioId + ']');
+
+    if (radio.checked) {
+        label.classList.add('question__answer-text_select_chose');
+    }
+
+    const filterIds = radioGroup[radio.name].filter(element => element !== radio.id);
+
+    filterIds.forEach(id => {
+        document.querySelector('label[for=' + id + ']').classList.remove('question__answer-text_select_chose');
+    });
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
-const filterBlockHead = document.querySelectorAll(".sidebar__block-head");
+const sidebarBlockHead = document.querySelectorAll(".sidebar__block-head");
 
-filterBlockHead.forEach(item => {
+sidebarBlockHead.forEach(item => {
 	item.addEventListener("click", function (evt) {
 		evt.stopPropagation();
 		item.parentNode.classList.toggle("sidebar__block_active");
