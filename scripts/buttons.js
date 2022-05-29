7b51ab505708bbd18ebfe550d50a96da52997bd2
Ivan Ershov <Navi133@yandex.ru>
2022-05-29 15:38:04 +0300

    Внесены  правки по результатам ревью

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -4,19 +4,24 @@ const route = {
 	index_next: '../test-preview.html',
 	test_preview_back: '../index.html',
 	test_preview_start: '../test.html',
+	view_conditions: '../about-test.html',
 	test_back: '../test-preview.html',
+	back_to_test: '../test.html',
 	about_test_back: '../test.html',
+	badtest_view_conditions: '../about-test.html',
 	test_reset: '../test.html',
 	badgoodtest_back: '../index.html',
+	goodtest_view_conditions: '../about-test.html',
 	goodtest_next: '../greet.html',
 	greet_back: '../goodtest.html',
 	greet_end: '../courses.html',
 	sorry_back: '../badtest.html',
 	sorry_end: '../courses.html',
+	test_choose_variant_view_conditions: '../about-test.html',
 	chose_variant_back: '../greet.html',
 	buttons: '../buttons.html',
 };
-const buttons = document.querySelectorAll('.btn');
+const buttons = document.querySelectorAll('button');
 
 buttons.forEach(button => {
 	const url = route[button.id];
